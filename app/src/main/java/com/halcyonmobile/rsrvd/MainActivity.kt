83ebd471584eb190b2a2dcc9d179ebac6ba82f0a
Biró Gellért <bgelli@halcyonmobile.com>
2017-11-28 13:15:50 +0200

    Initial commit

---
+++ app/src/main/java/com/halcyonmobile/rsrvd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.halcyonmobile.rsrvd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
