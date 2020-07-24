04247dd78dffd25d2a4e29996f357023177c7df9
Catalin-David <cata02dav@yahoo.com>
2020-07-24 17:08:49 +0300

    Added listener to bottom navigation view

--- app/src/main/java/com/halcyonmobile/rsrvd/MainActivity.kt
+++ app/src/main/java/com/halcyonmobile/rsrvd/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.halcyonmobile.rsrvd
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.halcyonmobile.rsrvd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +10,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val viewModel: MainViewModel by viewModels()
-
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.viewModel = viewModel
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+             when(menuItem.itemId){
+                R.id.navigation_profile -> {
+                    // TODO: navigate to profile fragment
+                    true
+                }
+                R.id.navigation_reservations -> {
+                    // TODO: navigate to reservations fragment
+                     true
+                }
+                R.id.navigation_explore -> {
+                    // TODO: navigate to explore fragment
+                     true
+                }
+                else -> false
+            }
+        }
     }
 }
