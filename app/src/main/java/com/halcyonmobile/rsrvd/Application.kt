c2be05dbecee09b611620373f64a40a50424504e
Catalin-David <cata02dav@yahoo.com>
2020-07-27 11:45:42 +0300

    Solved merge request comment

--- app/src/main/java/com/halcyonmobile/rsrvd/Application.kt
+++ app/src/main/java/com/halcyonmobile/rsrvd/Application.kt
@@ -2,11 +2,12 @@ package com.halcyonmobile.rsrvd
 
 import android.app.Application
 import com.halcyonmobile.rsrvd.core.repository.UserRepository
+import com.halcyonmobile.rsrvd.core.sharedpreferences.SharedPreferencesManager
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        UserRepository.init(applicationContext)
+        SharedPreferencesManager.init(applicationContext)
     }
 }
--- core/src/main/java/com/halcyonmobile/rsrvd/core/repository/UserRepository.kt
+++ core/src/main/java/com/halcyonmobile/rsrvd/core/repository/UserRepository.kt
@@ -1,14 +1,8 @@
 package com.halcyonmobile.rsrvd.core.repository
 
-import android.content.Context
 import com.halcyonmobile.rsrvd.core.sharedpreferences.SharedPreferencesManager
 
 object UserRepository {
-
-    fun init(context: Context) {
-        SharedPreferencesManager.init(context)
-    }
-
     var isUserLoggedIn: Boolean
         get() = SharedPreferencesManager.isUserLoggedIn
         set(userStatus) {
