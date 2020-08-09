370305ebea76ed8db3cade92fccb774cfbfd3f4f
iulia-android <iuliabirsan@yahoo.com>
2020-08-09 13:08:46 +0300

    eliminated unused imports

--- app/src/main/java/com/halcyonmobile/rsrvd/shared/FlexboxLayoutBindingAdapters.kt
+++ app/src/main/java/com/halcyonmobile/rsrvd/shared/FlexboxLayoutBindingAdapters.kt
@@ -3,8 +3,6 @@ package com.halcyonmobile.rsrvd.shared
 import androidx.databinding.BindingAdapter
 import com.google.android.flexbox.FlexboxLayout
 import com.halcyonmobile.rsrvd.core.shared.Facilities
-import com.halcyonmobile.rsrvd.onboarding.InterestView
-import com.halcyonmobile.rsrvd.core.shared.Interests
 import com.halcyonmobile.rsrvd.venuedetails.FacilityView
 
 @BindingAdapter("facilities")
